49040e80fe73521fba08cbba615a61b22ca93ddf
lkx31 <128697687+lkx31@users.noreply.github.com>
2023-06-15 23:22:39 +0800

    第八次
    第八次

---
+++ test8/BUTTONDlg.cpp
@@ -0,0 +1,34 @@
+﻿// BUTTONDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "holy.h"
+#include "afxdialogex.h"
+#include "BUTTONDlg.h"
+
+
+// BUTTONDlg 对话框
+
+IMPLEMENT_DYNAMIC(BUTTONDlg, CDialogEx)
+
+BUTTONDlg::BUTTONDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+BUTTONDlg::~BUTTONDlg()
+{
+}
+
+void BUTTONDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(BUTTONDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// BUTTONDlg 消息处理程序
